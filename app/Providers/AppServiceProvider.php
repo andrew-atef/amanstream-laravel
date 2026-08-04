97375c92e5f949dd9eb1266bfffc74e0cbe5ffa3
Andrew Atef <andrew.petr132@gmail.com>
2026-08-05 01:00:13 +0300

    solve

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use App\Observers\ArticleObserver;
 use App\Services\Amazon\Contracts\AmazonProductDataFetcher;
 use App\Services\Amazon\PlaceholderAmazonProductDataFetcher;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +28,10 @@ class AppServiceProvider extends ServiceProvider
         Blade::anonymousComponentPath(resource_path('views'));
 
         Article::observe(ArticleObserver::class);
+
+        // إجبار Laravel و Livewire على استخدام https في جميع الروابط والركام
+        if (config('app.env') !== 'local' || request()->header('X-Forwarded-Proto') === 'https') {
+            URL::forceScheme('https');
+        }
     }
 }
