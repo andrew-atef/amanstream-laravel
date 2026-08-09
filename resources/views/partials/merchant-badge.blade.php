5f0f97b2da709ac285beffe5db94b9fa84ba6bfe
Andrew Atef <andrew.petr132@gmail.com>
2026-08-10 00:49:44 +0300

    new

--- resources/views/partials/merchant-badge.blade.php
+++ resources/views/partials/merchant-badge.blade.php
@@ -1,4 +1,4 @@
-<div class="absolute bottom-1 right-1 flex items-center rounded-lg border border-slate-200 bg-white/95 px-2 py-1 shadow-sm backdrop-blur">
+<div class="absolute bottom-1 left-1 flex items-center rounded-lg border border-slate-200 bg-white/95 px-2 py-1 shadow-sm backdrop-blur">
     @if ($merchant === 'amazon')
         <img src="/icons/amazon.png" alt="Amazon" width="72" height="24" loading="lazy" class="h-6 w-auto object-contain">
     @elseif ($merchant === 'noon')
